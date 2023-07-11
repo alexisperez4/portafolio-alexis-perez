fb74f8789e1dad17023b9be195eee0c84ba0fe01
alexisperez4 <alexisperez4@gmail.com>
2023-07-11 03:08:07 -0400

    Add AppError class and custom error handling

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,9 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { taskRoutes } from './routes/tasks.routes';
-
-
 dotenv.config();
-
 const app: Express = express();
 const port = process.env.PORT;
-
-export class AppError extends Error {
-  statusCode: number;
-  message: string;
-
-  constructor(statusCode: number, message: string) {
-    super();
-    this.statusCode = statusCode;
-    this.message = message;
-  }
-}
 
 
 // Task Routes
@@ -26,13 +12,30 @@ app.use('/tasks', taskRoutes);
 // Handle errors
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Se produjo un error interno del servidor';
-  
-  res.status(statusCode).json({
-    status: "error",
-    statusCode,
-    message
-  });
+  switch (statusCode) {
+    case 400: // Bad Request
+      res.status(400).json({
+        status: "error",
+        statusCode,
+        message: err.message
+      });
+      break;
+    case 404: // Not Found
+      res.status(404).json({
+        status: "error",
+        statusCode,
+        message: err.message
+      });
+      break;
+    case 500: // Internal Server Error
+    default:
+      res.status(500).json({
+        status: "error",
+        statusCode,
+        message: err.message || 'Internal Server Error'
+      });
+      break;
+  }
 });
 
 
--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,20 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import { db } from "../database/database-config";
-import { AppError } from '../app';
+import { AppError } from '../utils/AppError';
 import { logger } from '../utils/logger';
 import { error } from 'console';
-
-  
 
 
 export const getAllTasks = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await db.query('SELECT * FROM tasks;');
+        const result = await db.query('SELECT * FROM task;');
         const tasks = result.rows;
         res.json(tasks);
     } catch (error) {
         logger.error(error);
-        next(new AppError(500, 'Se produjo un error interno del servidor'));
+        next(new AppError(500, 'Internal Server Error'));
     }
 };
 
