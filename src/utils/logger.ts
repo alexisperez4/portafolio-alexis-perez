87dfa524dbe5fb015dc7df18a1b9ba6b91a81da2
alexisperez4 <alexisperez4@gmail.com>
2023-07-22 12:05:03 -0400

    Updated logger configuration to suppress logs during test

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,32 @@
 import winston from 'winston';
 
+// to ignore info logs and only show errors in test environment
+const isTestEnvironment = process.env.NODE_ENV === 'test';
+
+const transports: winston.transport[] = [
+    new winston.transports.File({ 
+      filename: "./logs/app.log",
+    }),
+];
+  
+if (!isTestEnvironment) {
+    transports.push(
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.prettyPrint(),
+        ),
+      }),
+    );
+}
+
 export const logger = winston.createLogger({
-    level: "info",
+    level: 'info',
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         winston.format.errors({ stack: true }),
         winston.format.splat(),
         winston.format.printf((info) => `${info.timestamp} ${info.level} : [${info.stack}]`)
     ),
-    transports: [
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.prettyPrint(),
-
-            )
-        }),
-        new winston.transports.File({ 
-            filename: "./logs/app.log",
-        }),
-    ],
+    transports,
 });
-
