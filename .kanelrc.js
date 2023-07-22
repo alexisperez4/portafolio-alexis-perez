5b1e1ddfd39ed72bc51586456afa206785f53e1a
alexisperez4 <alexisperez4@gmail.com>
2023-07-21 21:44:00 -0400

    add kanel configs to generate types from postgres

---
+++ .kanelrc.js
@@ -0,0 +1,23 @@
+const path = require('path');
+require("dotenv").config({
+  path: path.join(__dirname, '/.env')
+});
+
+
+/** @type {import('kanel').Config} */
+module.exports = {
+  connection: {
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
+
+  preDeleteOutputFolder: true,
+  outputPath: './src/schemas',
+
+  customTypeMap: {
+    'pg_catalog.tsvector': 'string',
+    'pg_catalog.bpchar': 'string',
+  },
+};
