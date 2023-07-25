18c1ad85daef5b7a1eb3997d2f77215052c9b37d
alexisperez4 <alexisperez4@gmail.com>
2023-07-25 08:23:19 -0400

    Add role authorization middleeware to API routes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { verify } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { logger } from '../utils/logger';
+import UserRole from '../types/public/UserRole';
 
 // Extend Express' Request object to include a user property.
 declare global {
@@ -25,4 +26,20 @@ export const authenticateJWT = (req: Request, res: Response, next: NextFunction)
         logger.error(`Invalid Token. ${error}`);
         res.sendStatus(401);
     }
+};
+
+
+export const authorizeRoles = (roles: UserRole[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (!req.user) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        const hasRole = roles.indexOf(req.user.role) > -1;
+        if (!hasRole) {
+            return res.status(403).send('Forbidden');
+        }
+
+        next();
+    };
 };
--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,14 +1,29 @@
-import { Router, Request, Response } from 'express';
-import { db } from '../database/database-config';
+import { Router } from 'express';
 import { createTask, deleteTaskById, getAllTasks, getTaskById, updateTask } from '../controllers/tasks.controller';
+import { authorizeRoles } from '../middlewares/auth.middleware';
+import UserRole from '../types/public/UserRole';
 
 export const taskRoutes: Router = Router();
 
 taskRoutes.route('/')
-  .get(getAllTasks)
-  .post(createTask);
+  .get(
+    authorizeRoles([UserRole.admin, UserRole.user, UserRole.guest]),
+    getAllTasks
+  )
+  .post(
+    authorizeRoles([UserRole.admin, UserRole.user]),
+    createTask
+  );
 
 taskRoutes.route('/:task_id')
-  .get(getTaskById)
-  .delete(deleteTaskById)
-  .put(updateTask);
+  .get(
+    authorizeRoles([UserRole.admin, UserRole.user, UserRole.guest]),
+    getTaskById
+  )
+  .delete(
+    authorizeRoles([UserRole.admin]),
+    deleteTaskById
+  )
+  .put(authorizeRoles([UserRole.admin, UserRole.user]),
+    updateTask
+  );
